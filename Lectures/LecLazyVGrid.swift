64ee11dfdc495daefc8dcfa7ff19051166ae2339
Dilpreet Singh <dilpreetsinghhere@gmail.com>
2024-01-14 15:27:45 +0530

    Complete LazyVGrid code

--- Lectures/LecLazyVGrid.swift
+++ Lectures/LecLazyVGrid.swift
@@ -20,22 +20,44 @@ struct LecLazyVGrid: View {
     
     var body: some View {
         ScrollView{
+            
+            Circle()
+                .fill(Color.red)
+                .frame(width: .greatestFiniteMagnitude)
+            
             LazyVGrid(
                 columns: columns,
                 alignment: .center,
                 spacing: 6,
-                pinnedViews: /*@START_MENU_TOKEN@*/[]/*@END_MENU_TOKEN@*/,
+                pinnedViews: [.sectionHeaders],
                 content: {
                     Section(header: Text("Section 1")
-                        .foregroundStyle(Color.red)
+                        .foregroundStyle(Color.black)
                         .font(.title)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .background(Color.gray)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .background(Color.teal)
                         .padding()
                         
                             , content: {
                         ForEach(0..<50){ index in
                             Rectangle()
+                                .fill(Color.orange)
+                                .frame(height: 150)
+                                
+                        }
+                    })
+                    Section(header: Text("Section 2")
+                        .foregroundStyle(Color.black)
+                        .font(.title)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .background(Color.teal)
+                        .padding()
+                        
+                        
+                            , content: {
+                        ForEach(0..<50){ index in
+                            Rectangle()
+                                .fill(Color.brown)
                                 .frame(height: 150)
                         }
                     })
