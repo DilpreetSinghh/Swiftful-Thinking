bce7983a37791ef60c24cff90d40e9d71ff0bbd0
Dilpreet Singh <dilpreetsinghhere@gmail.com>
2024-01-16 00:47:34 +0530

    creating Extracting function and Views File

---
+++ Lectures/Lec_Func_and_Views.swift
@@ -0,0 +1,18 @@
+//
+//  Lec_Func_and_Views.swift
+//  Mastering SwiftUI
+//
+//  Created by Dilpreet Singh on 16/01/24.
+//
+
+import SwiftUI
+
+struct Lec_Func_and_Views: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Lec_Func_and_Views()
+}
