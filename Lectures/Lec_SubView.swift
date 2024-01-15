f09b92fb2925e556e7b5cdd1af747d82e967727a
Dilpreet Singh <dilpreetsinghhere@gmail.com>
2024-01-16 01:25:49 +0530

    adding lec extracting subview

---
+++ Lectures/Lec_SubView.swift
@@ -0,0 +1,21 @@
+//
+//  Lec_SubView.swift
+//  Mastering SwiftUI
+//
+//  Created by Dilpreet Singh on 16/01/24.
+
+
+import SwiftUI
+
+///  Extracting Subview
+struct Lec_SubView: View {
+    var body: some View {
+        ZStack{
+            Text("hi")
+        }
+    }
+}
+
+#Preview {
+    Lec_SubView()
+}
