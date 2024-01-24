1f666ea526077dc8810f9aa4281cbb161de1c878
Dilpreet Singh <dilpreetsinghhere@gmail.com>
2024-01-24 12:17:52 +0530

    List

--- Lectures/Lec_List.swift
+++ Lectures/Lec_List.swift
@@ -8,10 +8,75 @@
 import SwiftUI
 
 struct Lec_List: View {
+    
+    @State private var fruits: [String] = [
+        "apple", "orange", "banana", "grapes"
+    ]
+    
+    @State var veggies: [String] = [
+    "tomato", "potato", "carrot"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            List {
+                Section(header:
+                    HStack{
+                        Text("Fruits")
+                        Image(systemName: "apple.logo")
+                            .foregroundStyle(Color.red)
+                    }
+                ) {
+                    ForEach(fruits, id: \.self) { fruit in
+                        Text(fruit.capitalized)
+                    }
+                    .onDelete(perform: delete)
+                    .onMove(perform: move)
+
+                }
+                Section {
+                    ForEach(veggies, id: \.self){veggies in
+                        Text(veggies.capitalized)
+                    }
+                } header: {
+                    HStack {
+                        Text("Veggies")
+                        Image(systemName: "carrot.fill")
+                            .foregroundStyle(Color.orange)
+                    }
+                }
+
+            }
+            .listStyle(DefaultListStyle())
+            // have ability to change list style, make sure to use while making multiplatform application
+            .navigationTitle("Grocery List")
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                ToolbarItem(placement: .navigationBarTrailing){
+                    addButton
+                }
+            }
+        }
+    }
+    
+    var addButton: some View{
+        Button("Add") {
+            add()
+        }
+    }
+
+    func delete(indexSet: IndexSet) {
+        fruits.remove(atOffsets: indexSet)
+    }
+    func move(indices: IndexSet, newOffset: Int){
+        fruits.move(fromOffsets: indices, toOffset: newOffset)
+    }
+    func add(){
+        fruits.append("coconut".capitalized)
     }
 }
+
 
 #Preview {
     Lec_List()
