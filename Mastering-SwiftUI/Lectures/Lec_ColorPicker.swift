74129ab30deb192fae1417da999493d3148059f3
Dilpreet Singh <dilpreetsinghhere@gmail.com>
2024-03-08 23:52:59 +0530

    Creating Color Picker

---
+++ Mastering-SwiftUI/Lectures/Lec_ColorPicker.swift
@@ -0,0 +1,18 @@
+//
+//  Lec_ColorPicker.swift
+//  Mastering SwiftUI
+//
+//  Created by Dilpreet Singh on 08/03/24.
+//
+
+import SwiftUI
+
+struct Lec_ColorPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Lec_ColorPicker()
+}
