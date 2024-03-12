3972d475a7483b22d98ee6142a68e527fdf09f21
Dilpreet Singh <dilpreetsinghhere@gmail.com>
2024-03-12 18:41:01 +0530

    Swift Basics Course Added

---
+++ Mastering-SwiftUI/Lectures/Lec_TabView.swift
@@ -0,0 +1,18 @@
+//
+//  Lec_TabView.swift
+//  Mastering SwiftUI
+//
+//  Created by Dilpreet Singh on 11/03/24.
+//
+
+import SwiftUI
+
+struct Lec_TabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Lec_TabView()
+}
---
+++ Swift/Swift Basics.playground/Pages/BasicTypes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,4 @@
+import Foundation
+var greeting = "Hello, Children"
+
+// This is the comment
