699823e5d61d14e16d86e9c2081c4c69c8c2a85b
Dilpreet Singh <dilpreetsinghhere@gmail.com>
2024-03-26 23:51:38 +0530

    On appear although onDisappear also used but very basic level. Need more grinding

--- Mastering-SwiftUI/Lectures/Lec_OnAppear.swift
+++ Mastering-SwiftUI/Lectures/Lec_OnAppear.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct Lec_OnAppear: View {
+    
+    @State var myText: String = "Start text0"
+    @State var count: Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView{
+                Text(myText)
+                    .padding()
+                LazyVStack{
+                    ForEach(0..<50) { _ in
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 25.0)
+                                .frame(height: 250)
+                                .padding()
+                                .onAppear {
+                                    count += 1
+                            }
+                        }
+                    }
+                }
+            }
+            .onAppear(perform: {
+                /// Delayes the timing of action
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5){
+                    myText = "New text"
+                }
+                
+            })
+            .onDisappear(perform: {
+                myText = "End Text"
+            })
+            .navigationTitle("On Appear count: \(count)")
+        }
     }
 }
 
