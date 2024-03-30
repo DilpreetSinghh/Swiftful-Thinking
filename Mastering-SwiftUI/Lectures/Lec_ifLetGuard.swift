72d544ad482419e6e5fd472956d443cb5e1dd416
Dilpreet Singh <dilpreetsinghhere@gmail.com>
2024-03-30 09:41:47 +0530

    ifLetGuard

--- Mastering-SwiftUI/Lectures/Lec_ifLetGuard.swift
+++ Mastering-SwiftUI/Lectures/Lec_ifLetGuard.swift
@@ -8,10 +8,63 @@
 import SwiftUI
 
 struct Lec_ifLetGuard: View {
+    @State var currentUserID: String? = nil
+    @State var displayText: String? = nil
+    @State var isLoading: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("Here we are practicing safe coding!")
+                
+                if let text = displayText {
+                    Text(text)
+                        .font(.title)
+                }
+                
+//              DO NOT USER ! EVER!!!!!!!
+//              DO NOT FORCE UNWRAP VALUES
+//                Text(displayText!)
+//                    .font(.title)
+                
+                if isLoading {
+                    ProgressView()
+                }
+                
+                Spacer()
+            }
+            .navigationTitle("Safe Coding")
+            .onAppear {
+                loadData2()
+            }
+        }
     }
-}
+    
+    func loadData() {
+        if let userID = currentUserID {
+            isLoading = true
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                displayText = "This is the new data! User id is: \(userID)"
+                isLoading = false
+            }
+        } else {
+            displayText = "Error. There is no User ID!"
+        }
+    }
+    
+    func loadData2() {
+        
+        guard let userID = currentUserID else {
+            displayText = "Error. There is no User ID!"
+            return
+        }
+        
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            displayText = "This is the new data! User id is: \(userID)"
+            isLoading = false
+        }
+    }}
 
 #Preview {
     Lec_ifLetGuard()
