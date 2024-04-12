bf2f33eee86dc992e345524cdb846a3ccdf7c81b
Dilpreet Singh <dilpreetsinghhere@gmail.com>
2024-04-12 23:30:07 +0530

    ObservableObject and StateObject Before implementation of MVC

---
+++ Mastering-SwiftUI/Lectures/Lec_ViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  Lec_ViewModel.swift
+//  Mastering SwiftUI
+//
+//  Created by Dilpreet Singh on 12/04/24.
+//
+
+import SwiftUI
+
+struct Lec_ViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Lec_ViewModel()
+}
