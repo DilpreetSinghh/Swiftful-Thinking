98dcd6a2caa1062b53d9a6549d3f85fb26b83ea7
Dilpreet Singh <114622686+DilpreetSinghh@users.noreply.github.com>
2024-04-13 11:18:16 +0530

    Update Lec_ViewModel.swift

--- Mastering-SwiftUI/Lectures/Lec_ViewModel.swift
+++ Mastering-SwiftUI/Lectures/Lec_ViewModel.swift
@@ -7,9 +7,50 @@
 
 import SwiftUI
 
+struct FruitModel: Identifiable {
+    let id: String = UUID().uuidString
+    let name: String
+    let count: Int
+}
+
 struct Lec_ViewModel: View {
+    
+    @State var fruitArray: [FruitModel] = [
+        FruitModel(name: "Apple", count: 23)
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            List {
+                ForEach(fruitArray) { fruit in
+                    HStack{
+                        Text("\(fruit.count)")
+                            .foregroundStyle(.blue)
+                        Text(fruit.name)
+                            .font(.headline)
+                            .bold()
+                    }
+                }
+                .onDelete(perform: deleteFruit)
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Fruit List")
+            .onAppear{
+                getFruits()
+            }
+        }
+    }
+        //MARK: getFruit()
+    func getFruits(){
+        let fruit1 = FruitModel(name: "Orange", count: 45)
+        let fruit2 = FruitModel(name: "WaterMelon", count: 9)
+        
+        fruitArray.append(fruit1)
+        fruitArray.append(fruit2)
+    }
+    //MARK: deleteFruit
+    func deleteFruit(index: IndexSet){
+        fruitArray.remove(atOffsets: index)
     }
 }
 
